b02d944a47e59d9075af5837d114361bde7c9d12
Darren Cauthon <darren@cauthon.com>
2011-06-19 06:52:03 -0500

    Move logic for building a list of updatable values into a new class.

--- src/Shoon/SqlDenormalizer.cs
+++ src/Shoon/SqlDenormalizer.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Reflection;
 using Simple.Data;
-using Simple.Data.SqlServer;
 using SimpleCqrs.Eventing;
 
 namespace Shoon
@@ -13,11 +10,6 @@ namespace Shoon
         protected dynamic TheDatabaseTable
         {
             get { return Database.OpenConnection(GetTheConnectionString())["Products"]; }
-        }
-
-        protected IEnumerable<string> ColumnsInTheDatabaseTable
-        {
-            get { return GetTheColumnsInTheTable(); }
         }
 
         protected virtual void Insert(DomainEvent domainEvent)
@@ -52,54 +44,14 @@ namespace Shoon
             return ConfigurationManager.ConnectionStrings["Simple.Data.Properties.Settings.DefaultConnectionString"].ConnectionString;
         }
 
-        private static object GetValue(DomainEvent domainEvent, string column)
+        private static IDictionary<string, object> GetTheDataToUpdateInTheTable(DomainEvent domainEvent)
         {
-            return GetThePropertyOnThisObject(domainEvent, column).GetValue(domainEvent, null);
-        }
-
-        private static PropertyInfo GetThePropertyOnThisObject(object @object, string propertyName)
-        {
-            var type = @object.GetType();
-            return type.GetProperties()
-                .FirstOrDefault(x => x.Name == propertyName);
-        }
-
-        private Dictionary<string, object> GetTheDataToUpdateInTheTable(DomainEvent domainEvent)
-        {
-            var tableColumnsToUpdate = GetTheTableColumnsThatNeedToBeUpdated(domainEvent);
-
-            return BuildADataObjectThatHasAllUpdatableData(domainEvent, tableColumnsToUpdate);
-        }
-
-        private static Dictionary<string, object> BuildADataObjectThatHasAllUpdatableData(DomainEvent domainEvent,
-                                                                                          IEnumerable<string> tableColumnsToUpdate)
-        {
-            var dictionary = new Dictionary<string, object>();
-            foreach (var property in tableColumnsToUpdate)
-                dictionary[property] = GetValue(domainEvent, property);
-            dictionary["Id"] = domainEvent.AggregateRootId;
-            return dictionary;
-        }
-
-        private IEnumerable<string> GetTheTableColumnsThatNeedToBeUpdated(DomainEvent domainEvent)
-        {
-            return domainEvent.GetType().GetProperties()
-                .Select(x => x.Name)
-                .Where(property => ColumnsInTheDatabaseTable.Contains(property));
+            return (new UpdatableValuesBuilder().GetTheDataToUpdateInTheTable(domainEvent));
         }
 
         private bool ThisRecordHasBeenInserted(DomainEvent domainEvent)
         {
             return (bool) TheDatabaseTable.FindAllById(domainEvent.AggregateRootId).Any();
-        }
-
-        private static IEnumerable<string> GetTheColumnsInTheTable()
-        {
-            var connectionString = GetTheConnectionString();
-            var sqlConnectionProvider = new SqlConnectionProvider(connectionString);
-            var sqlSchemaProvider = new SqlSchemaProvider(sqlConnectionProvider);
-            var table = sqlSchemaProvider.GetTables().First();
-            return sqlSchemaProvider.GetColumns(table).Select(x => x.ActualName);
         }
     }
 }
---
+++ src/Shoon/UpdatableValuesBuilder.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using Simple.Data.SqlServer;
+using SimpleCqrs.Eventing;
+
+namespace Shoon
+{
+    public class UpdatableValuesBuilder
+    {
+        public IDictionary<string, object> GetTheDataToUpdateInTheTable(DomainEvent domainEvent)
+        {
+            var tableColumnsToUpdate = GetTheTableColumnsThatNeedToBeUpdated(domainEvent);
+
+            return BuildADataObjectThatHasAllUpdatableData(domainEvent, tableColumnsToUpdate);
+        }
+
+        protected IEnumerable<string> ColumnsInTheDatabaseTable
+        {
+            get { return GetTheColumnsInTheTable(); }
+        }
+
+        private IEnumerable<string> GetTheTableColumnsThatNeedToBeUpdated(DomainEvent domainEvent)
+        {
+            return domainEvent.GetType().GetProperties()
+                .Select(x => x.Name)
+                .Where(property => ColumnsInTheDatabaseTable.Contains(property));
+        }
+
+        private static IEnumerable<string> GetTheColumnsInTheTable()
+        {
+            var connectionString = GetTheConnectionString();
+            var sqlConnectionProvider = new SqlConnectionProvider(connectionString);
+            var sqlSchemaProvider = new SqlSchemaProvider(sqlConnectionProvider);
+            var table = sqlSchemaProvider.GetTables().First();
+            return sqlSchemaProvider.GetColumns(table).Select(x => x.ActualName);
+        }
+
+        private static Dictionary<string, object> BuildADataObjectThatHasAllUpdatableData(DomainEvent domainEvent,
+                                                                                          IEnumerable<string> tableColumnsToUpdate)
+        {
+            var dictionary = new Dictionary<string, object>();
+            foreach (var property in tableColumnsToUpdate)
+                dictionary[property] = GetValue(domainEvent, property);
+            dictionary["Id"] = domainEvent.AggregateRootId;
+            return dictionary;
+        }
+
+        private static object GetValue(DomainEvent domainEvent, string column)
+        {
+            return GetThePropertyOnThisObject(domainEvent, column).GetValue(domainEvent, null);
+        }
+
+        private static PropertyInfo GetThePropertyOnThisObject(object @object, string propertyName)
+        {
+            var type = @object.GetType();
+            return type.GetProperties()
+                .FirstOrDefault(x => x.Name == propertyName);
+        }
+
+        private static string GetTheConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["Simple.Data.Properties.Settings.DefaultConnectionString"].ConnectionString;
+        }
+    }
+}
